868c82a527929921cc47d0b1bb1787273efc56a2
Haoran Yi <haoran.yi@gmail.com>
2022-05-14 15:22:16 -0500

    init

---
+++ machine.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+export kin1=145.40.68.91
+export kin2=145.40.76.233
+export kin3=147.75.74.211
+export kin4=145.40.117.153
+
+export rpc1=34.148.201.98
+export rpc2=34.139.158.147
+
+export client1=34.139.150.67
+export client2=34.148.56.23
