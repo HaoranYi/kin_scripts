d2962cfebd22dfe3eea2c64e923022447d2ffa7b
Haoran Yi <haoran.yi@gmail.com>
2022-05-18 09:02:09 -0500

    up

--- machine.bat
+++ machine.bat
@@ -1,15 +1,15 @@
 @echo off
 
-set kin1=145.40.68.91
-set kin2=145.40.76.233
-set kin3=147.75.74.211
-set kin4=145.40.117.153
+set kin1=145.40.68.91        :: kin-validator-am6-1 / 31fxZovs3gBKVVTtC2VJUuKeVoq6mQkLjWnicWhErQ4f
+set kin2=145.40.76.233       :: kin-validator-da11-1 / Edfkf9gpC7KpnkNdKRPmseCtkE1zY8fUVRJMbiLYKKdK
+set kin3=147.75.74.211       :: kin-validator-ny5-1 / 2k31vk7hPiu2T9fJzuunc6tmaE57P7wt6tFoGK5A7k47
+set kin4=145.40.117.153      :: kin-validator-sg1-1 / 3cVWsRiTXD99BXNhzXs7Gkm3YBhCDrMQWnLern8B7TrD
 
-set rpc1=34.148.201.98
-set rpc2=34.139.158.147
+set rpc1=34.148.201.98       :: testnet-kin-rpc-bm-1 / 7JcmM6TFZMkcDkZe6RKVkGaWwN5dXciGC4fa3RxvqQc9
+set rpc2=34.139.158.147      :: testnet-kin-rpc-bm-2 / AYJDiE3wgw5eanU4qJ4qfkB8vrHVEiBMTqXLbA9hUTaW
 
-set client1=34.139.150.67
-set client2=34.148.56.23
+set client1=34.139.150.67    :: testnet-kin-accounts-client-bm-1
+set client2=34.148.56.23     :: testnet-kin-accounts-client-bm-2                                    
 
 
 set dev1=145.40.77.67
--- machine.sh
+++ machine.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
-export kin1=145.40.68.91
-export kin2=145.40.76.233
-export kin3=147.75.74.211
-export kin4=145.40.117.153
-
-export rpc1=34.148.201.98
-export rpc2=34.139.158.147
-
-export client1=34.139.150.67
-export client2=34.148.56.23
+export kin1=145.40.68.91               # kin-validator-am6-1 / 31fxZovs3gBKVVTtC2VJUuKeVoq6mQkLjWnicWhErQ4f
+export kin2=145.40.76.233              # kin-validator-da11-1 / Edfkf9gpC7KpnkNdKRPmseCtkE1zY8fUVRJMbiLYKKdK
+export kin3=147.75.74.211              # kin-validator-ny5-1 / 2k31vk7hPiu2T9fJzuunc6tmaE57P7wt6tFoGK5A7k47
+export kin4=145.40.117.153             # kin-validator-sg1-1 / 3cVWsRiTXD99BXNhzXs7Gkm3YBhCDrMQWnLern8B7TrD
+                                                                                                               
+export rpc1=34.148.201.98              # testnet-kin-rpc-bm-1 / 7JcmM6TFZMkcDkZe6RKVkGaWwN5dXciGC4fa3RxvqQc9
+export rpc2=34.139.158.147             # testnet-kin-rpc-bm-2 / AYJDiE3wgw5eanU4qJ4qfkB8vrHVEiBMTqXLbA9hUTaW
+                                                                                                               
+export client1=34.139.150.67           # testnet-kin-accounts-client-bm-1
+export client2=34.148.56.23            # testnet-kin-accounts-client-bm-2                                    
 
 
 export dev1=145.40.77.67
