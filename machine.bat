6567f5451a893a5819636195ba6f4a1796433c48
Haoran Yi <haoran.yi@gmail.com>
2022-05-18 18:39:06 -0500

    up

--- machine.bat
+++ machine.bat
@@ -1,19 +1,19 @@
 @echo off
 
-set kin1=145.40.68.91        :: kin-validator-am6-1 / 31fxZovs3gBKVVTtC2VJUuKeVoq6mQkLjWnicWhErQ4f
-set kin2=145.40.76.233       :: kin-validator-da11-1 / Edfkf9gpC7KpnkNdKRPmseCtkE1zY8fUVRJMbiLYKKdK
-set kin3=147.75.74.211       :: kin-validator-ny5-1 / 2k31vk7hPiu2T9fJzuunc6tmaE57P7wt6tFoGK5A7k47
-set kin4=145.40.117.153      :: kin-validator-sg1-1 / 3cVWsRiTXD99BXNhzXs7Gkm3YBhCDrMQWnLern8B7TrD
+set kin1=145.40.68.91        & :: kin-validator-am6-1 / 31fxZovs3gBKVVTtC2VJUuKeVoq6mQkLjWnicWhErQ4f
+set kin2=145.40.76.233       & :: kin-validator-da11-1 / Edfkf9gpC7KpnkNdKRPmseCtkE1zY8fUVRJMbiLYKKdK
+set kin3=147.75.74.211       & :: kin-validator-ny5-1 / 2k31vk7hPiu2T9fJzuunc6tmaE57P7wt6tFoGK5A7k47
+set kin4=145.40.117.153      & :: kin-validator-sg1-1 / 3cVWsRiTXD99BXNhzXs7Gkm3YBhCDrMQWnLern8B7TrD
 
-set rpc1=34.148.201.98       :: testnet-kin-rpc-bm-1 / 7JcmM6TFZMkcDkZe6RKVkGaWwN5dXciGC4fa3RxvqQc9
-set rpc2=34.139.158.147      :: testnet-kin-rpc-bm-2 / AYJDiE3wgw5eanU4qJ4qfkB8vrHVEiBMTqXLbA9hUTaW
+set rpc1=34.148.201.98       & :: testnet-kin-rpc-bm-1 / 7JcmM6TFZMkcDkZe6RKVkGaWwN5dXciGC4fa3RxvqQc9
+set rpc2=34.139.158.147      & :: testnet-kin-rpc-bm-2 / AYJDiE3wgw5eanU4qJ4qfkB8vrHVEiBMTqXLbA9hUTaW
 
-set client1=34.139.150.67    :: testnet-kin-accounts-client-bm-1
-set client2=34.148.56.23     :: testnet-kin-accounts-client-bm-2                                    
+set client1=34.139.150.67    & :: testnet-kin-accounts-client-bm-1
+set client2=34.148.56.23     & :: testnet-kin-accounts-client-bm-2                                    
 
 
-set dev1=145.40.77.67
-set dev2=147.75.38.57
-set dev3=139.178.94.233
-set dev4=147.75.84.251
-set dev5=145.40.71.95
+set dev1=145.40.77.67       & :: dev-server-da11
+set dev2=147.75.38.57       & :: dev-server-ny5
+set dev3=139.178.94.233     & :: dev-server-sv15
+set dev4=147.75.84.251      & :: dev-server-am6
+set dev5=145.40.71.95       & :: dev-server-sg1
