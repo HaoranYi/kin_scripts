a66964958554564fd5c386ce16d12f33444bce8f
Haoran Yi <haoran.yi@gmail.com>
2022-06-23 06:41:00 -0500

    up

---
+++ wipe_ledger.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash 
+set -ex
+
+LEDGER=./ledger
+
+rm -rf $LEDGER/accounts
+rm -rf $LEDGER/accounts_index
+rm -rf $LEDGER/rocksdb
+rm -rf $LEDGER/snapshot
+rm -rf $LEDGER/snapshot*.tar.zst
+rm -rf $LEDGER/incremental-snapshot*.zst
