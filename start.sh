4e194e98174ad94e491f9eeaf1b8569291d5cda0
haoran <haoran@mbook>
2022-07-23 09:18:55 -0500

    shell

--- start.sh
+++ start.sh
@@ -1,28 +1,28 @@
-@echo on
+#!/bin/bash
 
-plink -batch sol@%kin1% "~/restart"
-plink -batch sol@%kin1% "tail -f logs/solana-validator.log | grep -m 1 \"Waiting for\""
-
-
-plink -batch sol@%kin2% "~/restart"
-plink -batch sol@%kin3% "~/restart"
-plink -batch sol@%kin4% "~/restart"
+ssh sol@$kin1 "~/restart"
+ssh sol@$kin1 "tail -f logs/solana-validator.log | grep -m 1 \"Waiting for\""
 
 
-plink -batch sol@%kin1% "tail -f logs/solana-validator.log | grep -m 1 \"new root\""
-plink -batch sol@%kin2% "tail -f logs/solana-validator.log | grep -m 1 \"new root\""
-plink -batch sol@%kin3% "tail -f logs/solana-validator.log | grep -m 1 \"new root\""
-plink -batch sol@%kin4% "tail -f logs/solana-validator.log | grep -m 1 \"new root\""
+ssh sol@$kin2 "~/restart"
+ssh sol@$kin3 "~/restart"
+ssh sol@$kin4 "~/restart"
 
 
-plink -batch sol@%rpc1% "~/boot-scripts.sh"
-plink -batch sol@%rpc2% "~/boot-scripts.sh"
+ssh sol@$kin1 "tail -f logs/solana-validator.log | grep -m 1 \"new root\""
+ssh sol@$kin2 "tail -f logs/solana-validator.log | grep -m 1 \"new root\""
+ssh sol@$kin3 "tail -f logs/solana-validator.log | grep -m 1 \"new root\""
+ssh sol@$kin4 "tail -f logs/solana-validator.log | grep -m 1 \"new root\""
 
 
-plink -batch sol@%rpc1% "tail -f solana-validator-7JcmM6TFZMkcDkZe6RKVkGaWwN5dXciGC4fa3RxvqQc9.log | grep -m 1 \"new root\""
-plink -batch sol@%rpc2% "tail -f solana-validator-AYJDiE3wgw5eanU4qJ4qfkB8vrHVEiBMTqXLbA9hUTaW.log | grep -m 1 \"new root\""
+ssh sol@$rpc1 "~/boot-scripts.sh"
+ssh sol@$rpc2 "~/boot-scripts.sh"
 
 
-plink -batch sol@%rpc1% "~/run-client.sh"
-plink -batch sol@%rpc2% "~/run-client.sh"
+ssh sol@$rpc1 "tail -f solana-validator-7JcmM6TFZMkcDkZe6RKVkGaWwN5dXciGC4fa3RxvqQc9.log | grep -m 1 \"new root\""
+ssh sol@$rpc2 "tail -f solana-validator-AYJDiE3wgw5eanU4qJ4qfkB8vrHVEiBMTqXLbA9hUTaW.log | grep -m 1 \"new root\""
+
+
+ssh sol@$rpc1 "~/run-client.sh"
+ssh sol@$rpc2 "~/run-client.sh"
 
