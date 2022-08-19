9727083c0aab7424bbabdaf8421796105f4494a2
haoran <haoran@mbook>
2022-08-19 09:22:20 -0500

    up

--- influx-config.sh
+++ influx-config.sh
@@ -1,9 +1,7 @@
+#!/bin/bash
+
 # influx cloud UI
-https://us-west-2-2.aws.cloud2.influxdata.com/orgs/97bb031f366fc23d
+# https://us-west-2-2.aws.cloud2.influxdata.com/orgs/97bb031f366fc23d
 
 # config
-influx config create --active \ 
-    -n cloud \
-    -u https://us-west-2-2.aws.cloud2.influxdata.com \
-    -t RNAQuDTPvQ3f8LM3JSNfML5Gp4MLVya9kgtqWeK-nbY08EneJdcBIlcLfNkRxEPDW3_bWgIo5gV5Rka9qKNf6w== \
-    -o 97bb031f366fc23d 
+influx config create -a -n cloud -u https://us-west-2-2.aws.cloud2.influxdata.com -t RNAQuDTPvQ3f8LM3JSNfML5Gp4MLVya9kgtqWeK-nbY08EneJdcBIlcLfNkRxEPDW3_bWgIo5gV5Rka9qKNf6w== -o 97bb031f366fc23d 
