cf407efbec8f491d7a785a8e60ed61d731ad8a7f
haoran <haoran@mbook>
2022-09-22 12:34:22 -0500

    ggplot show

--- nb/ggplot_facet.py
+++ nb/ggplot_facet.py
@@ -1,10 +1,20 @@
+import time
+#import threading
+
 from plotnine.data import mpg
 from plotnine import ggplot, aes, facet_grid, labs, geom_point, theme_dark
 
 from matplotlib import pyplot as plt
 
-# 2-d facet grid with (year, class) for one plot 
 
+def close_all(): 
+    print('close in 5 seconds')
+    time.sleep(5)
+    plt.close('all')
+    print('done')
+
+
+# 2-d facet grid with (year, class) for one plot 
 p = (
     ggplot(mpg)
     + facet_grid(facets="year~class")
@@ -19,8 +29,27 @@ p = (
 )
 
 # save the plot
-p.save('myplot.png', dpi=600)
+#p.save('myplot.png', dpi=600)
 
-fig = p.draw()
-#print(type(fig))
-plt.show()
+# render the plot (when running from the python command line)
+(fig, plot) = p.draw(return_ggplot=True, show=True)
+
+
+# NOT WORKING
+# threading.Thread(target=close_all) 
+
+# NOT WORKING EITHER (on mac)
+# set the timer interval 5000 milliseconds
+# (fig, plot) = p.draw(return_ggplot=True, show=False)
+# timer = fig.canvas.new_timer(interval = 3000)
+# timer.add_callback(close_all)
+# timer.start()
+# plt.figure()
+# fig.show()
+# plt.show()
+
+
+# matplot way of rendering
+# plt.draw()
+# fig = plt.figure()
+# plt.show()
