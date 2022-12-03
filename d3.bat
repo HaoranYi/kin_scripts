c75bba88ecff12c1a14797dc6985b37f077c7da6
Haoran Yi <haoran.yi@gmail.com>
2022-12-02 19:09:12 -0600

    up

--- d3.bat
+++ d3.bat
@@ -1,4 +1,21 @@
-FOR /F "tokens=* USEBACKQ" %%F IN (`command`) DO (
+@echo off
+
+:: example to execute a command and save the result into variables
+
+
+FOR /F "tokens=* USEBACKQ" %%F IN (`echo abc`) DO (
 SET var=%%F
 )
 ECHO %var%
+
+
+SETLOCAL ENABLEDELAYEDEXPANSION
+SET count=1
+FOR /F "tokens=* USEBACKQ" %%F IN (`dir .`) DO (
+  SET var!count!=%%F
+  SET /a count=!count!+1
+)
+ECHO %var1%
+ECHO %var2%
+ECHO %var3%
+ENDLOCAL
