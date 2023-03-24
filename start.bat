48ee4b6a7629cd9000d94b0bf2587350aa238837
Haoran Yi <haoran.yi@gmail.com>
2023-03-23 19:14:36 -0500

    up

--- start.bat
+++ start.bat
@@ -26,13 +26,13 @@ timeout 10
 plink -batch sol@%rpc1% "~/boot-scripts.sh"
 plink -batch sol@%rpc2% "~/boot-scripts.sh"
 
-timeout 10
+timeout 650
 plink -batch sol@%rpc1% "tail -F solana-validator-7JcmM6TFZMkcDkZe6RKVkGaWwN5dXciGC4fa3RxvqQc9.log | grep -m 1 \"new root\""
 plink -batch sol@%rpc2% "tail -F solana-validator-AYJDiE3wgw5eanU4qJ4qfkB8vrHVEiBMTqXLbA9hUTaW.log | grep -m 1 \"new root\""
 
 
 echo "starting clients ..."
-timeout 30
+timeout 650
 :: plink -batch sol@%client1% "~/run-client.sh"
 :: plink -batch sol@%client2% "~/run-client.sh"
 plink -batch sol@%client1% "~/restart"
