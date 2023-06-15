6065d8098a4e5358e77d62f0f53b58df862c82e4
Haoran Yi <haoran.yi@gmail.com>
2023-06-15 08:14:29 -0500

    up

--- kill.bat
+++ kill.bat
@@ -7,17 +7,17 @@ plink -batch sol@%kin4% "~/stop"
 plink -batch sol@%bm_rpc1% "sudo pkill -9 -f solana-validator"
 plink -batch sol@%bm_rpc2% "sudo pkill -9 -f solana-validator"
 
-plink -batch sol@%client1% "~/stop"
-plink -batch sol@%client2% "~/stop"
+plink -batch sol@%bm_rpc1% "~/stop"
+plink -batch sol@%bm_rpc2% "~/stop"
 
 plink -batch sol@%kin1% "systemctl status sol"
 plink -batch sol@%kin2% "systemctl status sol"
 plink -batch sol@%kin3% "systemctl status sol"
 plink -batch sol@%kin4% "systemctl status sol"
-plink -batch sol@%rpc1% "ps -aux | grep solana-validator"
-plink -batch sol@%rpc2% "ps -aux | grep solana-validator" 
+plink -batch sol@%bm_rpc1% "ps -aux | grep solana-validator"
+plink -batch sol@%bm_rpc2% "ps -aux | grep solana-validator" 
 
-plink -batch sol@%client1% "systemctl status kin.client"
-plink -batch sol@%client2% "systemctl status kin.client"
+plink -batch sol@%bm_rpc1% "systemctl status kin.client"
+plink -batch sol@%bm_rpc2% "systemctl status kin.client"
 
 
