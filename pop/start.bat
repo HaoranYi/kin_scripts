7323090f8193aa21fb873dbcd96cd4e87499b9b8
Haoran Yi <haoran.yi@gmail.com>
2023-07-06 10:03:58 -0500

    up

--- pop/start.bat
+++ pop/start.bat
@@ -21,16 +21,16 @@ plink -batch sol@%pn2% "tail -F logs/solana-validator.log | grep -m 1 \"new root
 plink -batch sol@%pn3% "tail -F logs/solana-validator.log | grep -m 1 \"new root\""
 plink -batch sol@%pn4% "tail -F logs/solana-validator.log | grep -m 1 \"new root\""
 
-exit 0
-
 echo "starting rpc nodes ..."
 timeout 10
 plink -batch sol@%pn_rpc1% "~/boot-scripts.sh"
 plink -batch sol@%pn_rpc2% "~/boot-scripts.sh"
 
+goto end
+
 timeout 60
-plink -batch sol@%pn_rpc1% "tail -F solana-validator-7JcmM6TFZMkcDkZe6RKVkGaWwN5dXciGC4fa3RxvqQc9.log | grep -m 1 \"new root\""
-plink -batch sol@%pn_rpc2% "tail -F solana-validator-AYJDiE3wgw5eanU4qJ4qfkB8vrHVEiBMTqXLbA9hUTaW.log | grep -m 1 \"new root\""
+plink -batch sol@%pn_rpc1% "tail -F solana-validator-*.log | grep -m 1 \"new root\""
+plink -batch sol@%pn_rpc2% "tail -F solana-validator-*.log | grep -m 1 \"new root\""
 
 
 echo "starting clients ..."
@@ -40,4 +40,5 @@ timeout 60
 plink -batch sol@%pn_rpc1% "~/restart"
 plink -batch sol@%pn_rpc2% "~/restart"
 
+:end
 echo "done"
