d7551f2e2fb5516951bf4c5b79e7312cf39e9205
haoran <haoran@mbook>
2023-08-01 14:42:28 -0500

    up

--- start.sh
+++ start.sh
@@ -1,14 +1,12 @@
 #!/bin/bash
 set -ex
 
-
-echo "starting leader kin nodes ..."
+echo "====1 starting leader kin node ..."
 ssh sol@$kin1 "~/restart"
 ssh sol@$kin1 "tail -f logs/solana-validator.log | grep -m 1 \"Waiting for\""
+echo "leader started"
 
-sleep 60
-
-echo "starting other kin nodes ..."
+echo "====2 starting other validator nodes ..."
 ssh sol@$kin2 "~/restart"
 ssh sol@$kin3 "~/restart"
 ssh sol@$kin4 "~/restart"
@@ -19,9 +17,9 @@ ssh sol@$kin1 "tail -F logs/solana-validator.log | grep -m 1 \"new root\""
 ssh sol@$kin2 "tail -F logs/solana-validator.log | grep -m 1 \"new root\""
 ssh sol@$kin3 "tail -F logs/solana-validator.log | grep -m 1 \"new root\""
 ssh sol@$kin4 "tail -F logs/solana-validator.log | grep -m 1 \"new root\""
+echo "cluster new rooted"
 
-
-echo "starting rpc nodes ..."
+echo "====3 starting rpc nodes ..."
 ssh sol@$bm_rpc1 "~/boot-scripts.sh"
 ssh sol@$bm_rpc2 "~/boot-scripts.sh"
 
@@ -29,7 +27,9 @@ ssh sol@$bm_rpc2 "~/boot-scripts.sh"
 ssh sol@$bm_rpc1 "tail -F solana-validator-7JcmM6TFZMkcDkZe6RKVkGaWwN5dXciGC4fa3RxvqQc9.log | grep -m 1 \"new root\""
 ssh sol@$bm_rpc2 "tail -F solana-validator-AYJDiE3wgw5eanU4qJ4qfkB8vrHVEiBMTqXLbA9hUTaW.log | grep -m 1 \"new root\""
 
-echo "starting clients ..."
+echo "rpc nodes started"
+
+echo "====4 starting clients ..."
 sleep 30
 #ssh sol@$client1 "~/run-client.sh"
 #ssh sol@$client2 "~/run-client.sh"
