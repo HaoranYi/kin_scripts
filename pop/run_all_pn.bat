d47da544f726d5e416f335e0237f8bcc2e9aabb5
Haoran Yi <haoran.yi@gmail.com>
2023-08-09 10:24:59 -0500

    up

--- pop/run_all_pn.bat
+++ pop/run_all_pn.bat
@@ -1,9 +1,24 @@
 @echo on
-set nodes=%pn1% %pn2% %pn3% %pn4% %pn_rpc1% %pn_rpc2%
+set nodes=%kin1% %kin2% %kin3% %kin4% %bm_rpc1% %bm_rpc2%
+::set nodes=%pn1% %pn2% %pn3% %pn4% %pn_rpc1% %pn_rpc2%
 ::set nodes=%pn2% %pn3% %pn4% 
 
 ::set cmd="cp /etc/hostname ~/.hostname"
-set cmd="ls -l /home/sol/ledger/*.zst"
+::set cmd="ls -l /home/sol/ledger/*.zst"
+
+::perf tools
+::set cmd="sudo DEBIAN_FRONTEND=noninteractive apt-get -yq install linux-tools-common linux-tools-generic linux-tools-`uname -r`"
+:: -- adjust root right to run perf-tools
+::set cmd="sudo sh -c 'echo 1 >/proc/sys/kernel/perf_event_paranoid'"
+:: set cmd="sudo sysctl -w kernel.perf_event_paranoid=1"  
+:: To ensure this is done at boot time create your own
+:: /etc/sysctl.d/99-mysetting.conf file with
+::   kernel.perf_event_paranoid=1
+:: choose a filename that won't override existing file in `/run/sysctrl.d` and `/usr/lib/sysctrl.d/`
+:: see man sysctrl.d for details
+::
+
+set cmd="sudo perf stat -e page-faults --timeout 5000"
 
 for %%a in (%nodes%) do (
      plink -batch sol@%%a %cmd% 
